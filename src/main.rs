c49a6fc11053ed7ff1b9a8255801774943e9a437
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-07-09 19:57:03 +0100

    Define possible moves for queens

--- src/main.rs
+++ src/main.rs
@@ -265,7 +265,57 @@ fn get_possible_moves(
 
     match piece_type {
         Piece::King => {}
-        Piece::Queen => {}
+        Piece::Queen => {
+            for i in 0..8 {
+                let ex_pos = match i {
+                    0 => (0, 1),
+                    1 => (0, -1),
+                    2 => (1, 0),
+                    3 => (-1, 0),
+                    4 => (1, 1),
+                    5 => (1, -1),
+                    6 => (-1, 1),
+                    7 => (-1, -1),
+                    _ => unreachable!(),
+                };
+
+                let mut path = true;
+                let mut chain = 1;
+
+                let (allies_positions, enemies_positions) = get_allies_and_enemies(
+                    piece_player,
+                    &white_pieces_positions,
+                    &black_pieces_positions,
+                );
+
+                while path {
+                    if !allies_positions.contains(&&BoardPosition::new(
+                        piece_position.x + ex_pos.0 * chain,
+                        piece_position.y + ex_pos.1 * chain,
+                    )) && piece_position.x + ex_pos.0 * chain >= 0
+                        && piece_position.x + ex_pos.0 * chain <= 7
+                        && piece_position.y + ex_pos.1 * chain >= 0
+                        && piece_position.y + ex_pos.1 * chain <= 7
+                    {
+                        possible_moves.push((
+                            piece_position.x + ex_pos.0 * chain,
+                            piece_position.y + ex_pos.1 * chain,
+                        ));
+
+                        if enemies_positions.contains(&&BoardPosition::new(
+                            piece_position.x + ex_pos.0 * chain,
+                            piece_position.y + ex_pos.1 * chain,
+                        )) {
+                            path = false;
+                        }
+
+                        chain += 1;
+                    } else {
+                        path = false;
+                    }
+                }
+            }
+        }
         Piece::Knight => {
             let targets = [
                 (1, 2),
